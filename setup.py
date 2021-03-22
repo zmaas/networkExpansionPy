dee4987769765bb1af51655d3c4939ad86362298
Zach Maas <zach.maas@gmail.com>
2021-03-22 14:15:17 -0600

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-	name = "networkExpansionPy",
-	version = '0.0',
-	description = 'Python package containing algorithms for simulating the expansion of metabolic networks',
-	url = 'https://github.com/jgoldford/networkExpansionPy/',
-	author = 'Joshua E. Goldford',
-	author_email = 'goldford.joshua@gmail.com',
-	packages = find_packages(),
-	install_requires = [],
-	include_package_data = True,
+    name="networkExpansionPy",
+    version="0.0",
+    description="Python package containing algorithms for simulating the expansion of metabolic networks",
+    url="https://github.com/jgoldford/networkExpansionPy/",
+    author="Joshua E. Goldford",
+    author_email="goldford.joshua@gmail.com",
+    packages=find_packages(),
+    install_requires=["numpy", "pandas", "networkx", "webweb"],
+    include_package_data=True,
 )
