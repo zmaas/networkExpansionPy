4590f45053ac8911edb33495260a37f53cb35d44
ClairHuffine <clair@DESKTOP-UN7F9TE.localdomain>
2021-04-08 17:20:59 -0600

    Added test for expansion sizes

--- examples/expansion_functional_tests.sh
+++ examples/expansion_functional_tests.sh
@@ -13,11 +13,7 @@ test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest
 run test_example python -c "print ('example assert_in_stdout success')"
 assert_in_stdout "example"
 
+
 run test_europa python expand_europa.py
 assert_exit_code 0
-
-run test_minimal python expand_minimal.py
-assert_exit_code 0
-
-run test_metals python expand_metals.py
-assert_exit_code 0
+assert_in_stdout "Total compounds: 32 Total reactions: 34"
