94646b2e4033d2d8058b365900a39e0968199f18
ClairHuffine <clair.huffine@colorado.edu>
2021-04-19 10:08:51 -0600

    Added alternate Atlas settings

--- examples/expand_minimal_oxidized.py
+++ examples/expand_minimal_oxidized.py
@@ -1,11 +1,14 @@
 import networkExpansionPy.lib as ne
 import networkExpansionPy.parseKegg as pk
+import networkExpansionPy.parseAtlas as pa
 import pandas as pd
 from webweb import Web
+import matplotlib.pyplot as plt
+import numpy as np
 
 # initializing network and pruning
-metabolism = ne.GlobalMetabolicNetwork()
-metabolism.init_pruning()
+metabolism = ne.GlobalMetabolicNetwork(atlas=True)
+metabolism.init_pruning(atlas=True)
 metabolism.oxygen_indepentend()
 
 # define seed compounds
@@ -27,7 +30,7 @@ print("----------------------------------------------------------------------")
 #print("----------------------------------------------------------------------")
 
 # plotting
-rxn_pairs, _ = pk.get_rxn_pairs()
+rxn_pairs, _ = pa.get_rxn_pairs()
 
 edges = set()
 
@@ -48,6 +51,34 @@ with open("../networkExpansionPy/assets/iqbio/compounds.csv") as cpds_file_handl
             line.strip("cpd:").strip().split(maxsplit=1) for line in lines
         ]
     }
+
+# Get the number of new compounds at each step
+rxns_count_list = [len(x) for x in ne_rxns_list]
+rxns_count_differences = list(
+    np.array(rxns_count_list[1:]) - np.array(rxns_count_list[:-1])
+)
+rxns_count_stepratios = list(
+    np.array(rxns_count_differences) / np.array(rxns_count_list[:-1])
+)
+rxns_count_logquots = list(
+    np.log2(np.array(rxns_count_list[1:]) / np.array(rxns_count_list[:-1]))
+)
+
+# Plot the growth over time
+fig, ax = plt.subplots(nrows=2, ncols=2)
+fig.suptitle("Per-Iteration Growth on Minimal Expansion Set")
+ax[0, 0].plot(rxns_count_list)
+ax[0, 0].set_xlabel("Expansion Iteration")
+ax[0, 0].set_ylabel("Total Reactions")
+ax[0, 1].plot(rxns_count_differences)
+ax[0, 1].set_xlabel("Expansion Iteration")
+ax[0, 1].set_ylabel("Reactions Added")
+ax[1, 0].plot(rxns_count_stepratios)
+ax[1, 0].set_xlabel("Expansion Iteration")
+ax[1, 0].set_ylabel("Reactions Added / Previous Network Size")
+ax[1, 1].plot(rxns_count_logquots)
+ax[1, 1].set_xlabel("Expansion Iteration")
+ax[1, 1].set_ylabel("Log2(Relative Size Increase)")
 
 edges_renamed = [(cpd_dict[x], cpd_dict[y]) for x, y in edges]
 nodes_renamed = [cpd_dict[x] for x in ne_cpds]
